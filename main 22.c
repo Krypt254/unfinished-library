802c8ee1f57e02f75bc3db109a20ade0cfe102db
Antony Njuguna <98992978+Krypt254@users.noreply.github.com>
2022-03-04 13:45:21 +0300

    Add files via upload

---
+++ main 22.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+
+struct patron {
+     char name[100];
+     char pass[30];
+     char email[50]
+     int is_staff;
+};
+
+void add_patron() {
+    struct patron patron;
+    printf("Enter name:");
+    gets(patron.name);
+    printf("Enter Email:");
+    gets(patron.email);
+    printf("Initial Password:");
+    gets(patron.pass);
+    printf("Enter 1 if staff 0 otherwise:");
+    scanf("%d",&patron.is_staff);
+    print("Patron %s successfully added\n",patron.name)
+     }
+int menu();
+void execute_action(int action);
+int main()
+{
+    int action;
+    action = menu();
+    execute_action(action);
+    return 0;
+}
+
+void execute_action(int action){
+    printf("You selected action %d", action);
+}
+
+int menu() // header/prototype
+{
+   int action;
+    do {
+        printf("\tCounty Library System\n");
+        printf("Welcome Mr.Antony. \n");
+        printf("What would you like to do?\n");
+        printf("1. View Users. \n");
+        printf("2. Add New User. \n");
+        printf("3. Edit User. \n");
+        printf("4. Delete User. \n");
+        printf("5. Change Password. \n");
+        printf("6. Log out. \n");
+        printf("7. Exit System. \n");
+        printf("Selected Action(1-7): ");
+        scanf("%d", &action);
+
+        if (action < 1 || action > 7){
+            system("cls");
+            printf("Invalid action!!! Try again.\n");
+        }
+    }while(action < 1 || action > 7);
+
+switch(action)
+    {
+    case 1:
+        printf("list of all Users.\n");
+    break;
+
+    case 2:
+        printf("Enter Name of the New User.\n");
+    break;
+
+    case 3:
+        printf("Enter Name of the User to be Edited.\n");
+    break;
+
+    case 4:
+        printf("Enter Name of the User to be Deleted.\n");
+    break;
+
+    case 5:
+        printf("Enter New Password.\n");
+    break;
+
+    case 6:
+        printf("Log Out.\n");
+    break;
+
+    case 7:
+        printf("Exit System.\n");
+    break;
+
+    }
+
+}
